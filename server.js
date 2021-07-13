2c0483691eca53ac7d95d5580d3692b63246f73e
Felipe Villegas <felavid@gmail.com>
2021-07-13 18:25:05 -0400

    Challenge completed.

--- server.js
+++ server.js
@@ -11,44 +11,24 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    /*Store hash in your db*/
+    console.log(hash);
 
-
-
+        bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        /*res == true or false*/
+        console.log(res);
+        });
+});
 //END_ASYNC
-
+  
 //START_SYNC
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
 
-
-
+const result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result)
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(process.env.PORT || 3000, () => {});
